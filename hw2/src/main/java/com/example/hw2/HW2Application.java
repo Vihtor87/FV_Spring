e79eb1b43253be192ca927cceda7ce3b25817a42
Viktor Vinogradov <viktor.vinogradov@list.ru>
2024-11-25 21:33:19 +0300

    change

--- hw2/src/main/java/com/example/hw2/HW2Application.java
+++ hw2/src/main/java/com/example/hw2/HW2Application.java
@@ -1,7 +1,9 @@
 package com.example.hw2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class HW2Application {
 
     public static void main(String[] args) {
