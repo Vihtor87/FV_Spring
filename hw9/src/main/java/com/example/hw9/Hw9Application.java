e689aa4ee682f1c890e950b736a045df8bb9e473
Viktor Vinogradov <viktor.vinogradov@list.ru>
2024-12-23 15:22:40 +0300

    change

--- hw9/src/main/java/com/example/hw9/Hw9Application.java
+++ hw9/src/main/java/com/example/hw9/Hw9Application.java
@@ -9,4 +9,12 @@ public class Hw9Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Hw9Application.class, args);
 	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocator builder) {
+		return builder.routes()
+				.route("Microservice1",r->r.path("/serviceA/**"))
+				.uri("http://localhost:8081/")
+				.build();
+	}
 }
