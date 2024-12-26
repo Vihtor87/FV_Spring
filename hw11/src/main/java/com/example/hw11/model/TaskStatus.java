4c4ef39c8a51c979614d42b7ff017b9f3eae5a8b
Viktor Vinogradov <viktor.vinogradov@list.ru>
2024-12-26 18:03:27 +0300

    add hw11

---
+++ hw11/src/main/java/com/example/hw11/model/TaskStatus.java
@@ -0,0 +1,20 @@
+package com.example.hw11.model;
+
+public enum TaskStatus {
+    NOT_STARTED {
+        @Override
+        public String toString() {
+            return "Не начата";
+        }
+    }, IN_PROGRESS {
+        @Override
+        public String toString() {
+            return "В процессе";
+        }
+    }, COMPLETED {
+        @Override
+        public String toString() {
+            return "Завершена";
+        }
+    };
+}
